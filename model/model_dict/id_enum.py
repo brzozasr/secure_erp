1ecc46dde491e29f564d317050f005583ae15870
Sławomir Brzozowski <brzozasr@gmail.com>
2020-08-09 14:51:52 +0200

    New files: model/model_dict/crm.py, model/model_dict/hr.py, model/model_dict/id_enum.py, model/model_dict/id_store.py, model/model_dict/products.py, model/model_dict/sales.py

---
+++ model/model_dict/id_enum.py
@@ -0,0 +1,9 @@
+from enum import Enum
+# from model.model_dict.id_store import *
+
+
+class ID(Enum):
+    CRM = "id_crm_store"
+    PRODUCT = "id_prod_store"
+    SALE = "id_sale_store"
+    HR = "id_hr_store"
