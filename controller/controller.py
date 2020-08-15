79fa3313385a02d5d530a2eba7756790f69dc646
Sławomir Brzozowski <brzozasr@gmail.com>
2020-08-15 21:16:22 +0200

    Update controller.py

--- controller/controller.py
+++ controller/controller.py
@@ -9,6 +9,14 @@ class Controller:
                "len_company_crm": (2, 25),
                "len_email_crm": (6, 35)}
 
+    len_hr = {"len_id_hr": (1, 6),
+              "len_name_hr": (2, 20),
+              "len_surname_hr": (2, 30),
+              "len_birthday_hr": (10, 10),
+              "len_department_hr": (2, 20),
+              "len_email_hr": (6, 35),
+              "len_password": (40, 40)}
+
     def __init__(self, model, view):
         self.model = model
         self.view = view
@@ -20,7 +28,21 @@ class Controller:
             len_max.append(cls.len_crm[key][1])
         return len_max
 
+    @classmethod
+    def len_max_hr(cls):
+        len_max = []
+        for key, value in cls.len_hr.items():
+            len_max.append(cls.len_hr[key][1])
+        return len_max
+
 
 if __name__ == "__main__":
     con = Controller("test", "test1")
     print(con.len_max_crm())
+    print(con.len_max_hr())
+
+    # import hashlib
+    #
+    # hash_object = hashlib.sha1(b'Hello World')
+    # hex_dig = hash_object.hexdigest()
+    # print(hex_dig)
